0b92d39c1a72728fc88e647b111127803abb7807
anik554 <67149943+anik554@users.noreply.github.com>
2025-06-17 18:31:43 +0600

    manual, package and zod validation add

--- src/app/controllers/users.controller.ts
+++ src/app/controllers/users.controller.ts
@@ -1,16 +1,40 @@
 import express, { Request, Response } from "express";
 import { User } from "../models/users.model";
+import { z } from "zod";
 
 export const userRouter = express.Router();
 
+const UserZodSchema = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  age: z.number(),
+  email: z.string(),
+  password: z.string(),
+  role: z.string().optional(),
+  address: z.object({
+    city: z.string(),
+    street: z.string(),
+    zip: z.number(),
+  }),
+});
+
 userRouter.post("/create-user", async (req: Request, res: Response) => {
-  const data = req.body;
-  const user = await User.create(data);
-  res.status(201).json({
-    success: true,
-    message: "User has been created successfully!",
-    user,
-  });
+  try {
+    const data = await UserZodSchema.parseAsync(req.body);
+    const user = await User.create(data);
+    res.status(201).json({
+      success: true,
+      message: "User has been created successfully!",
+      user,
+    });
+  } catch (error: any) {
+    console.log(error);
+    res.status(400).json({
+      success: false,
+      message: error.message,
+      error,
+    });
+  }
 });
 
 userRouter.get("/get-users", async (req: Request, res: Response) => {
--- src/app/models/users.model.ts
+++ src/app/models/users.model.ts
@@ -1,13 +1,58 @@
 import { model, Schema } from "mongoose";
-import { IUser } from "../interfaces/user.interface";
+import { IAddress, IUser } from "../interfaces/user.interface";
+import validator from "validator";
+
+const addressSchema = new Schema<IAddress>(
+  {
+    city: { type: String },
+    street: { type: String },
+    zip: { type: Number },
+  },
+  {
+    _id: false,
+  }
+);
 
 const userSchema = new Schema<IUser>(
   {
-    firstName: { type: String, required: true, trim: true },
+    firstName: {
+      type: String,
+      required: [true, `First name must not be empty`],
+      minlength: [3, `First Name minimum 3 charracters but got {VALUE}`],
+      maxlength: [10, `First Name maximum 10 charracters but got {VALUE}`],
+      trim: true,
+    },
     lastName: { type: String, trim: true },
-    email: { type: String, required: true, trim: true },
+    age: { type: Number, required: true, min: 18, max: 60 },
+    email: {
+      type: String,
+      required: true,
+      unique: [true, "Email already exists"],
+      lowercase: true,
+      trim: true,
+
+      // manula custom validator
+      // validate: {
+      //   validator: function (value) {
+      //     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value);
+      //   },
+      //   message: (props) => `${props.value} is not a valid email address.`,
+      // },
+
+      // using package custom validator
+      validate: [validator.isEmail, "Invalid Email Send {VALUE}"],
+    },
     password: { type: String, required: true },
-    role: { type: String, enum: ["user", "admin"], default: "user" },
+    role: {
+      type: String,
+      uppercase: true,
+      enum: {
+        values: ["USER", "ADMIN", "SUPERADMIN"],
+        message: "Role is not valid, got {VALUE}",
+      },
+      default: "SUPERADMIN",
+    },
+    address: { type: addressSchema },
   },
   {
     versionKey: false,
